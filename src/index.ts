e5726fcd4cf8a62fdf383e576638305825cb201b
Harshana-Prabhath <harshanasemasinghe@gmail.com>
2026-06-28 19:43:44 +0530

    cors issue fixed

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["http://localhost:5173", "http://localhost:3000"]; 
+
+app.use(cors(
+  {
+    origin: (origin, callback) => {
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Blocked by secure cross-origin resource sharing policy."));
+      }
+    },
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }
+
+));
 app.use(express.json());
 
 
